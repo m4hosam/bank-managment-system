6e48281bfafa96d5ec5be74613fbb730c42e7586
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2022-05-07 00:52:51 +0300

    OOP update

--- Python Code/classes.py
+++ Python Code/classes.py
@@ -7,6 +7,21 @@ class Account:
         self.currency = account.currency
         self.balance = account.balance
 
+    # Not Tested yet
+    def get_account(self, account_id):
+        self.account_id = account_id
+        cursor.execute(
+            '''SELECT a.acc_id, a.currency, a.balance 
+            FROM account As a 
+            WHERE a.acc_id = ?''', self.account_id)
+        row = cursor.fetchone()
+        if(not row):
+            print("Account not Found")
+        else:
+            self.balance = row.balance
+            self.currency = row.currency
+        return self
+
     def save(self):
         cursor.execute(
             '''INSERT INTO account 
@@ -18,6 +33,33 @@ class Account:
             '''DELETE account 
             WHERE id = ?''', self.account_id)
         connection.commit()
+
+    def get_balence(self):
+        return self.balance
+
+    def get_currency(self):
+        return self.currency
+
+    def update_balance(self, value):
+        self.balance = value
+        cursor.execute(
+            '''UPDATE account
+            SET account.balance = ?
+            WHERE account.acc_id = ?''', self.balance, self.account_id)
+        connection.commit()
+
+    def money_transfer(self, receiver, amount):
+        new_balance = int(self.balance) - int(amount)
+        # Exchange rate logic should be here
+        receiver_balance = receiver.balance + amount
+
+        if(new_balance >= 0):
+            self.update_balance(new_balance)
+            cursor.execute(
+                '''UPDATE account
+                 SET account.balance = ?
+                 WHERE account.acc_id = ?''', receiver_balance, receiver.account_id)
+            connection.commit()
 
     def __str__(self):
         return f"----\n({self.account_id}, {self.currency},{self.balance})\n----"
@@ -44,7 +86,9 @@ class Customer:
 
     def list_accounts(self):
         cursor.execute(
-            'SELECT a.acc_id, a.currency, a.balance FROM account As a, userAccounts As u WHERE u.acc_id = a.acc_id AND u.cus_id = ?', self.customer_id)
+            '''SELECT a.acc_id, a.currency, a.balance 
+            FROM account As a, userAccounts As u 
+            WHERE u.acc_id = a.acc_id AND u.cus_id = ?''', self.customer_id)
         rows = cursor.fetchall()
         for row in rows:
             self.accounts.append(Account(row))
@@ -60,7 +104,7 @@ class Customer:
             c.id = ?
             ORDER BY(t.trans_date)''', self.customer_id)
         rows = cursor.fetchall()
-        print(rows)
+        # print(rows)
         for row in rows:
             self.transactions.append(row)
         return self.transactions
--- Python Code/project.py
+++ Python Code/project.py
@@ -66,10 +66,10 @@ def ATM_operations(type):
     # else:
         # cursor.execute('Update', account_no, )
 
-    print("Withdraw Completed")
     # if balance - amount is negative (Can't Withdraw)
     # Else: Change the database balance into the new value
     # Message Withdraw completed
+    print("Withdraw Completed")
 
 
 # ATM_operations("Withdraw")
@@ -77,65 +77,65 @@ def ATM_operations(type):
 
 def open_account():
     # list all accounts that belong to the user ---(Query)
-    Currency = input("Currency: ")
     # Insert A row in the account table with the new account ---(Query)
     # Message Request has been made for your new account
+    Currency = input("Currency: ")
 
 
 def delete_account():
     # list all accounts that belong to the user ---(Query)
-    account_no = input("Account No: ")
     # Get the balance of the selected account    --(Query)
     # If the balance not 0 CAn not be deleted
     # Else: Message Request has been sent for deleting (Clerk)
+    account_no = input("Account No: ")
 
 
 def update_information():
     # list all information that belong to the user ---(Query)
     # Option to Edit all the information ---(Interface)
-    edit = input("All can be edited: ")
     # Submit a request to the clerk
     # Message Request has been sent for Updating (Clerk)
+    edit = input("All can be edited: ")
 
 
 def money_transfer():
     # list all accounts that belong to the user ---(Query)
-    # Option to Edit all the information ---(Interface)
-    account_no = input("Select an account to transfer from: ")
-    recieve_account = input("Select an account to transfer to: ")
+    # account_no = input("Select an account to transfer from: ")
+    # recieve_account = input("Select an account to transfer to: ")
     # Select the accounts from database
     # Depending on exchange rate, calculations will be occur
     # Change the balance of the selected account
     # Message Money has been transferred
+    account_no = input("Select an account to transfer from: ")
 
 
 def monthly_summery():
     # Filter all transactions of that user -order by date-  ---(Query)
-    total_transaction = 0
     # list them on the Screen
     # Consider the loans in a separate section
+    total_transaction = 0
 
 
 def exchange_rate_panel():
     # Select all currencies with their exchange rate  ---(Query) Currency Table
-    total_currencies = 0
     # list them on the Screen
     # Consider the loans in a separate section
+    total_currencies = 0
 
 
 def list_accounts():
     # Select all corresponding accounts to the selected user ---(Query) account Table
-    total_accounts = 0
     # list them on the Screen
+    total_accounts = 0
 
 
 def bankGeneralStatus():
-    income = 0
     # -income-  select total money that has been deposited into the bank (paying loan included)
     # -expenses- select both withdraw and loan money ----(Sum Query)
     # -Profit-  select total profit out from loans
     # -total balance- Sum of the balance of all accounts
     # Message all info to the Manager Screen
+    income = 0
 
 
 def addNewCurrency():
@@ -143,37 +143,37 @@ def addNewCurrency():
     # select all currencies out of currency table for comparing
     # if the new currency already exists don't add message User
     # else add to the currency table
-    currencyValue = input("insert the currency value: ")
     # Message Success or Failure
+    currencyValue = input("insert the currency value: ")
 
 
 def updateCurrency():
     currency = input("insert the currency")
     # select all currencies out of currency table for comparing
     # if the currency exists
-    currencyValue = input("insert the currency value: ")
     # else ask for redirection to add new currency
     # Message Success or redirection
+    currencyValue = input("insert the currency value: ")
 
 
 def updateEmployeeSalary():
-    newSalary = input("insert new salary")
     # Update the clerk table with the new Salary
     # Message Success for Manager screen
+    newSalary = input("insert new salary")
 
 
 def updateInterestRate():
-    interest = input("insert new interest Rate: ")
     # Update the Interest Rate with the new Data
     # Message Success for Manager screen
+    interest = input("insert new interest Rate: ")
 
 
 def addNewCustomers():
-    newCustomers = 0
     # Select the all new customers from ---(Query) new Customer Table
     # For Every Customer move it to the customers table and assign a clerk for him
     # -Assigning clerk- it is assigned to the clerk with the least number of customers.
     # append the customer id to the responsible clerk ----(Clerk Table)
+    newCustomers = 0
 
 
 # print("1-Withdraw\n"
