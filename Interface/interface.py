9519ac4f7fd9f391b396a2f4e86644ae7fc35ed5
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2022-05-08 15:57:55 +0300

    Rename the classes customer-manager

--- Interface/interface.py
+++ Interface/interface.py
@@ -1,37 +1,96 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+
+from managerMain import Ui_ManagerWindow
+from customerMain import Ui_CustomerWindow
 
 
-class MyWindow(QMainWindow):
+class CustomerWindow:
     def __init__(self):
-        super(MyWindow, self).__init__()
-        self.setGeometry(200, 200, 300, 300)
-        self.setWindowTitle("Bank Managment System")
-        self.initUI()
+        self.main_win = QMainWindow()
+        self.ui = Ui_CustomerWindow()
+        self.ui.setupUi(self.main_win)
 
-    def initUI(self):
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText("my first label")
-        self.label.move(50, 50)
+        self.ui.stackedWidget.setCurrentWidget(self.ui.PListAccounts)
 
-        self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText("click me")
-        self.b1.clicked.connect(self.clicked)
+        self.ui.moneyTransfer.clicked.connect(self.showMoneyTransfer)
+        self.ui.deposit.clicked.connect(self.showDeposit)
+        self.ui.withdraw.clicked.connect(self.showWithdraw)
+        self.ui.listAccounts.clicked.connect(self.showListAccounts)
+        self.ui.newAccount.clicked.connect(self.showCreateAccount)
+        self.ui.deleteAccount.clicked.connect(self.showDeleteAccount)
 
-    def clicked(self):
-        self.label.setText("You Pressed The button")
-        self.update()
+    def show(self):
+        self.main_win.show()
 
-    def update(self):
-        self.label.adjustSize()
+    def showMoneyTransfer(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.PMoneyTransfer)
+
+    def showDeposit(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.PDeposit)
+
+    def showWithdraw(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.PWithdraw)
+
+    def showListAccounts(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.PListAccounts)
+
+    def showCreateAccount(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.PCreateAccount)
+
+    def showDeleteAccount(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.PDeleteAccount)
 
 
-def window():
+class ManagerWindow:
+    def __init__(self):
+        self.main_window = QMainWindow()
+        self.ui = Ui_ManagerWindow()
+        self.ui.setupUi(self.main_window)
+
+        self.ui.stackedWidget.setCurrentWidget(self.ui.main_widget)
+
+        self.ui.addNewCurrency.clicked.connect(self.showAddNewCurrency)
+        self.ui.updateCurrency.clicked.connect(self.showUpdateCurrency)
+        self.ui.update_clerk.clicked.connect(self.showUpdate_clerk)
+        self.ui.update_interest.clicked.connect(self.showUpdate_interest)
+        self.ui.add_customer.clicked.connect(self.showAdd_customer)
+        self.ui.view_transactions.clicked.connect(self.showView_transactions)
+
+    def show(self):
+        self.main_window.show()
+
+    def showAddNewCurrency(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.add_currency_widget)
+
+    def showUpdateCurrency(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.update_currency_widget)
+
+    def showUpdate_clerk(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.update_clerk_widget)
+
+    def showUpdate_interest(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.update_interest_widget)
+
+    def showAdd_customer(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.add_customer_widget)
+
+    def showView_transactions(self):
+        self.ui.stackedWidget.setCurrentWidget(
+            self.ui.view_transactions_widget)
+
+
+type = input("1-Customer\n2-Manager\n3-Clerk")
+if type == '1':
     app = QApplication(sys.argv)
-    win = MyWindow()
-    win.show()
+    main_win = CustomerWindow()
+    main_win.show()
     sys.exit(app.exec_())
 
 
-window()
+if type == '2':
+    app = QApplication(sys.argv)
+    main_window = ManagerWindow()
+    main_window.show()
+    sys.exit(app.exec_())
