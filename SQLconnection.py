d6487f4f1dc8436c8309de541b6c7d6d9c39145f
Mohamed Hosam <79701103+m4hosam@users.noreply.github.com>
2022-05-11 02:58:07 +0300

    Delete SQLconnection.py

--- SQLconnection.py
+++
@@ -1,9 +0,0 @@
-import pyodbc
-
-connection = pyodbc.connect('Driver={SQL Server};'
-                            'Server=DESKTOP-5CILQ1E;'
-                            'Database=Bank;'
-                            'Trusted_Connection=yes;'
-                            )
-
-cursor = connection.cursor()
