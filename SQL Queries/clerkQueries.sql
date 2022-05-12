54e9b8efcf10673fc048bf3dea2fff9abe4424b8
Marah Alasi <77880165+MSR-layer@users.noreply.github.com>
2022-05-13 02:24:47 +0300

    Update clerkQueries.sql

--- SQL Queries/clerkQueries.sql
+++ SQL Queries/clerkQueries.sql
@@ -77,40 +77,71 @@ SELECT a.acc_id
 					cusClrk.clrk_id = 3);*/
 
 
---SELECTS ALL TRANSACTIONS WHERE THE SOURCE IS AN ACCOUNT BELONGING TO ONE OF THE CLERK'S CUSTOMERS, ALSO DISPLAYS THE CUS_ID AND ACCOUNT BALANCE
-/*SELECT tr.trans_date,tr.src_id as src_acc, trans_type, total, ua.cus_id, a.balance
+--SELECTS ALL TRANSACTIONS + SOURCE AND RECIEVE CUSTOMER ID AND BALANCE
+SELECT DISTINCT trans_date, ua1.cus_id src_cus, ua2.cus_id rsv_cus, src_id, rsv_id, trans_type, total, (a1.balance - total) src_balance, (a2.balance + total *(ISNULL(c1.exch_rate,c2.exch_rate) / (ISNULL(c2.exch_rate,c1.exch_rate)))) as rsv_balance 
 FROM transactions2 tr
-LEFT JOIN userAccounts2 ua
-ON ua.acc_id = tr.src_id 
-LEFT JOIN account2 a
-ON a.acc_id = tr.src_id
-WHERE tr.src_id IN(SELECT ua.acc_id
-		FROM userAccounts2 ua, customer2 c
-		WHERE c.id = ua.cus_id and
-			ua.cus_id IN (SELECT c.id
-					FROM customer2 c, customerClerks2 cc
-					WHERE c.id = cc.cus_id and
-						clerk_id = {clerk_id}))
-*/
+LEFT JOIN account2 a1
+ON tr.src_id = a1.acc_id
+LEFT JOIN account2 a2
+ON tr.rsv_id = a2.acc_id
+LEFT JOIN userAccounts2 ua1
+ON ua1.acc_id = src_id
+LEFT JOIN userAccounts2 ua2
+ON ua2.acc_id = rsv_id
+LEFT JOIN currency c1
+ON c1.curr_code = a1.currency
+LEFT JOIN currency c2
+ON c2.curr_code = a2.currency
+WHERE ua1.cus_id = {cus_id} or ua2.cus_id = {cus_id};
+
 
 --expense
-SELECT DISTINCT ua.cus_id, tr.*, total * exch_rate
+SELECT DISTINCT ua.cus_id, SUM(total * ISNULL(exch_rate,1))
 FROM transactions2 tr, currency curr, account2 a, userAccounts2 ua
 WHERE ua.acc_id = tr.src_id and
-	tr.src_id = a.acc_id and
-	a.currency = curr.curr_code;
+tr.src_id = a.acc_id and
+a.currency = curr.curr_code
+and (rsv_id NOT IN (SELECT acc_id
+		FROM userAccounts2 ua2 
+		WHERE ua.cus_id = ua2.cus_id)
+OR rsv_id IS NULL) and ua.cus_id IN(SELECT cc.cus_id
+				FROM customerClerks2 cc
+				WHERE ua.cus_id = cc.cus_id and
+				cc.clerk_id = {self.clerk_id})
+GROUP BY(ua.cus_id) ORDER BY ua.cus_id;
 
 --income
-SELECT DISTINCT ua.cus_id, tr.*, total * exch_rate
-FROM transactions2 tr, currency curr, account2 a, userAccounts2 ua
-WHERE ua.acc_id = tr.rsv_id and
-	tr.rsv_id = a.acc_id and
-	a.currency = curr.curr_code;
+SELECT t1.cus_id, SUM(ISNULL(t1.income, 0) + ISNULL(t2.income,0))
+FROM (SELECT ua2.cus_id , SUM(total * c1.exch_rate) income
+FROM transactions2 tr, account2 a1, account2 a2, userAccounts2 ua1, userAccounts2 ua2, currency c1, currency c2
+WHERE tr.src_id = a1.acc_id and
+tr.rsv_id = a2.acc_id and
+ua1.acc_id = src_id and
+ua2.acc_id = rsv_id and
+c1.curr_code = a1.currency and 
+c2.curr_code = a2.currency and src_id not in(SELECT acc_id
+					FROM userAccounts2 ua
+					WHERE ua.cus_id = ua2.cus_id)
+GROUP BY(ua2.cus_id)) t1
+FULL OUTER JOIN (
+SELECT ua1.cus_id, SUM(total * c1.exch_rate) income
+FROM transactions2 tr, account2 a1, userAccounts2 ua1, currency c1
+WHERE src_id IS NULL and
+rsv_id = ua1.acc_id and
+ua1.acc_id = a1.acc_id and
+a1.currency = c1.curr_code
+GROUP BY(ua1.cus_id)) t2 on t2.cus_id = t1.cus_id, customerClerks2 cc
+WHERE t1.cus_id = cc.cus_id and
+cc.clerk_id = {self.clerk_id}
+GROUP BY(t1.cus_id) ORDER BY t1.cus_id;
 
 --total balance
-SELECT c.id,/*, a.acc_id,*/ SUM(exch_rate * balance)  as totalBalance
+SELECT c.id, SUM(exch_rate * balance)  as totalBalance
 FROM currency cur, userAccounts2 ua, account2 a, customer2 c
 WHERE ua.acc_id = a.acc_id and
-		a.currency = cur.curr_code and
-		c.id = ua.cus_id
-GROUP BY(c.id)
+a.currency = cur.curr_code and
+c.id = ua.cus_id and ua.cus_id IN(SELECT cc.cus_id
+				FROM customerClerks2 cc
+				WHERE ua.cus_id = cc.cus_id and
+				cc.clerk_id = {self.clerk_id})
+GROUP BY(c.id) ORDER BY c.id
